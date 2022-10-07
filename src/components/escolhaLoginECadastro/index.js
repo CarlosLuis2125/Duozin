5b1d8b75933e06ea8cfdfe253ccfa1b1fb806389
LUIS CARLOS MENDES DA SILVA <luis.cmsilva9@senacsp.edu.br>
2022-10-07 19:38:40 -0300

    segundo commit

---
+++ src/components/escolhaLoginECadastro/index.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, n, } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.principal}>
+      <View style={styles.container}>
+        <Text>Logo Duozin</Text>
+        <StatusBar style="auto" />
+        
+      </View>
+      <View style={styles.loginCadastro}>
+        <Button title='Login' onPress={''}></Button>
+        
+        <Button title='Cadastrar' onPress={''}></Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  principal: {
+    flex: 2,
+    backgroundColor: '#444',
+    
+  },
+  loginCadastro: {
+    flex: 3,
+    backgroundColor: '#fff',
+    fontSize: 50,
+  
+  },
+});
