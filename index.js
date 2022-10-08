9b8a45c832f023deda7b7241ab1e9777f271729d
LUIS CARLOS MENDES DA SILVA <luis.cmsilva9@senacsp.edu.br>
2022-10-07 22:12:40 -0300

    arrumando tempo da tela de login

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button , TextInput} from 'react-native';
-import Fundo from "./images/FundoLoginCadastro.png";
+import { StyleSheet, Text, View, Button, TextInput, ImageBackground } from 'react-native';
+import Fundo from './images/FundoLoginCadastro.png';
 
-export default function Login(){
-    return (
-            <View style={styles.container}>
-                <Fundo/>
+
+const Login = () => (
+  <View style={styles.container}>
+            <ImageBackground source={Fundo} resizeMode="cover" style={styles.image}>
                 <Text>Login</Text>
                 <Text>Logo Duozin</Text>
                 <StatusBar style="auto" />
-                
+
                 <View style={styles.email}>
                     <TextInput style={styles.input} value={Text} placeholder='Email'></TextInput>
                 </View>
                 <View style={styles.senha}>
-                    <TextInput style={styles.input}  value={Text} placeholder='Senha'></TextInput>
+                    <TextInput style={styles.input} value={Text} placeholder='Senha'></TextInput>
                 </View>
                 <View style={styles.botaoVotar}>
                     <Button title='Voltar' onPress={''}></Button>
@@ -29,9 +29,12 @@ export default function Login(){
                     <Button title='Voltar' onPress={''}></Button>
                     <Button title='Avançar' onPress={''}></Button>
                 </View>
-            </View>
-    );
-}
+            </ImageBackground>
+
+    </View>
+  
+
+  );
 
 const styles = StyleSheet.create({
     container: {
@@ -50,6 +53,6 @@ const styles = StyleSheet.create({
         float: 'right',
     },
     botaoVotarAvancar: {
-        float: 'left', 
+        float: 'left',
     }
 });
