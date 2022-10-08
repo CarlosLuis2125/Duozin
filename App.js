91b67077e06f163b47bca49bdc667455a7232a61
VINICIUS EDUARDO VICENTE RAFAEL <vinicius.evrafael@senacsp.edu.br>
2022-10-07 21:10:58 -0300

    [FIX]Commit pre-merge

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, n, } from 'react-native';
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, Text, Button, View, Alert } from 'react-native';
+import fundo from './images/FundoPrincipal.png';
 
-export default function App() {
-  return ();
-}
+const App = () => (
+  <View style={styles.container}>
+    <ImageBackground source={require('./images/FundoPrincipal.png')} style={styles.imageBackground}>
+      <Text style={styles.text}>React Native</Text>
+      <Button style={styles.botao}
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+    </ImageBackground>
+  </View>
+
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+});
+
+export default App;
